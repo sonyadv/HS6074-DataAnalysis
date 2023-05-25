d269e103af25a5d213922379d49e150a61460058
Sonya :D <0509yanting@gmail.com>
2023-05-25 10:16:20 +0800

    Update monthly.m

--- monthly.m
+++ monthly.m
@@ -6,6 +6,21 @@ rho = 1024; % kg/m^3
 A = r^2*pi; % m^2
 Cp = 0.35;
 
+cd ..
+
+% % read the .xyz file and seprate into three array: lx, ly, lz
+% taipd = load('Taidp200m.xyz');
+% lx = taipd(:,1);
+% ly = taipd(:,2);
+% lz = taipd(:,3);
+% 
+% % reshape 1-dim arrays into 2-dim martix
+% bath_x = reshape(lx,2001,2501);
+% bath_y = reshape(ly,2001,2501);
+% depth = reshape(lz,2001,2501);
+
+cd('codar_data')
+
 %% plot codar data
 for month = 9:9
         % set up the grid
@@ -15,15 +30,18 @@ for month = 9:9
             
     % count
     power = zeros(size(X));
-    count = zeros(size(X));
+    
 
 
     for day = 15:30
-
-            %% initialize
+            
 
         for hour = 0:23
-    
+
+            % initialize
+            count = zeros(size(X));
+            p = zeros(size(X));
+
             % read files
             fn=['TOTL_ALLM_2014_' sprintf('%02d',month) '_' sprintf('%02d',day) '_' sprintf('%02d',hour) '00' '.tuv'];
             q = load(fn);
@@ -65,10 +83,13 @@ for month = 9:9
                 end
 				
                % memo
-               power(r,c) = power(r,c)+vel(i);
+               p(r,c) = p(r,c)+vel(i);
                count(r,c) = count(r,c)+1;
             end
-        
+
+            p = p./count;
+            p(isnan(p)) = 0;
+            power = power+p;
         % end of hour
         end
 
@@ -76,16 +97,18 @@ for month = 9:9
     end
 
 
-        power = power./count;
+        %power = power./count;
+        %power(isnan(power)) = 0;
+        power(power == 0) = NaN;
         power = power.*rho*A*Cp; % vel^3 to power
-        power = power.*3600; % everysec
-        power = power.*(10^-3); % J to kJ
+        power = power.*3600; % everyhour
+        power = power.*(10^-3); % W to kW
 			
         % plot power
         m_proj('lambert','lat',[21 26.5],'long',[118.5 123.5]);
         hold on
         m_pcolor(X,Y,power);
-        caxis([0 10]);
+        %caxis([0 10]);
         hold on
         m_gshhs_i('color','k');
         
@@ -98,6 +121,7 @@ for month = 9:9
         %hold on
         
         % plot coastline
+%         contourf(bath_x,bath_y,depth,[0 0],'k','linewidth',1.3) 
         m_grid('box','fancy','tickdir','in');
         date = [sprintf('%02d',month) '/' sprintf('%02d',day)];
         title("September");
